319e4c0b53d5b65d7ff15c1456e83b0c34ea1f87
Nikita Chernyshov <xpom55@mail.ru>
2017-11-08 15:15:46 +0300

    material-ui and firebase connected

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import firebase from 'firebase';
+
+import AutoComplete from 'material-ui/AutoComplete';
 
 class App extends Component {
+  componentWillMount() {
+    // Initialize Firebase
+    const config = {
+      apiKey: "AIzaSyAqj3ulJmKs2318t-5UkZzGw59NGzAMieM",
+      authDomain: "fatherproject3.firebaseapp.com",
+      databaseURL: "https://fatherproject3.firebaseio.com",
+      projectId: "fatherproject3",
+      storageBucket: "fatherproject3.appspot.com",
+      messagingSenderId: "554574798495"
+    };
+    firebase.initializeApp(config);
+
+    const user = {
+      email: 'test@test.com',
+      password: 'password',
+    };
+
+    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+      .then(() => console.log('auth successFUL!!!'))
+      .catch((err) => {
+        console.log('auto Error', err);
+      });
+
+    firebase.database().ref(`/test/test/article`)
+      .push({ test: 'test' })
+      .then(() => {
+        console.log('pushed');
+      })
+      .catch((err) => {
+        console.log('error!', err);
+      });
+  }
+
+  handleUpdateInput = (value) => {
+
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+          <h1>Welcome to React 3 33</h1>
+
+          <AutoComplete
+            hintText="Date"
+            dataSource={['123','222', '4443']}
+            onUpdateInput={this.handleUpdateInput}
+          />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import App from './App';
+
+const renderApp = () => {
+  return (
+    <MuiThemeProvider>
+      <App />
+    </MuiThemeProvider>
+  );
+}
+
+ReactDOM.render(renderApp(), document.getElementById('root'));
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    ReactDOM.render(renderApp(),
+      document.getElementById('root'),
+    )
+  })
+}
